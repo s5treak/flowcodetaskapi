017ac7378d39ea662b4ad2579ef7a6632b24fd91
Prosper <prosper.kalu@outlook.com>
2020-06-18 15:39:14 +0100

    finished endpoint function in controller to show/update a movie

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -80,5 +80,112 @@ class MovieController extends Controller
     
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $movie = Movie::find($id);
+        
+        $msg = is_null($movie) ? 'Movie is not available' : $movie;
+
+        $status = is_null($movie) ? 404 : 200;
+
+        return response()->json([
+            
+            'message'=> $msg,
+            
+        ], $status);
+    }
+
+ 
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {   
+        if(Movie::where('id', $id)->exists()) {
+
+           $validator = Validator::make($request->all(), $this->rules('update'), $this->errors());
+        
+       
+           if($validator->fails())
+            {
+              return response()->json([
+                 
+                'message' => $validator->messages()->all()
+    
+              ],422);
+            }
+
+            $movie = Movie::find($id);
   
+            if($request->hasFile('cover')) {
+               
+               
+                $this->delete_picture($movie->cover);
+                
+                $fileNameToStore = $this->upload_picture($request->cover);
+          
+                $movie->update(
+  
+                  [
+  
+                    'title' => $request->title,
+                    'genre' => is_array($request->genre) == 1 ? $request->genre : explode(" ",$request->genre),
+                    'country_of_production'=> $request->country_of_production,
+                    'description'=> $request->description,
+                    'cover'=> $fileNameToStore
+  
+                  ]
+  
+                );
+  
+            }
+
+           
+
+            $movie->update(
+
+                [
+
+                    'title' => $request->title,
+                    'genre' => is_array($request->genre) == 1 ? $request->genre : explode(" ",$request->genre),
+                    'country_of_production'=> $request->country_of_production,
+                    'description'=> $request->description,
+
+                ]
+            );
+
+              
+
+            return response()->json([
+                 
+                'message'=> 'Movie Details Has been Updated',
+      
+            ],200);
+
+        }else{
+
+            return response()->json([
+                "message" => "Movie not found"
+            ], 404);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    
 }
