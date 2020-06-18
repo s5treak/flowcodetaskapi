8087294761bcac4fd233e38dd35ef4b103651c88
Prosper <prosper.kalu@outlook.com>
2020-06-18 15:37:38 +0100

    finished endpoint function to store movie

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -33,6 +33,52 @@ class MovieController extends Controller
 
     }
  
- 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+               
+        $validator = Validator::make($request->all(), $this->rules('create'), $this->errors());
+        
+       
+           if($validator->fails())
+            {
+              return response()->json([
+                 
+                'message' => $validator->messages()->all()
     
+              ],422);
+            }
+
+          
+          
+            $fileNameToStore = $this->upload_picture($request->cover);
+          
+            Movie::create([
+
+                'title' => $request->title,
+                'genre' => is_array($request->genre) == 1 ? $request->genre : explode(" ",$request->genre),
+                'country_of_production'=> $request->country_of_production,
+                'description'=> $request->description,
+                //'main_video'=> 'required|mimes:mp4|max:10000',
+                'cover'=> $fileNameToStore
+
+
+            ]);
+
+            return response()->json([
+                
+                'message'=> 'Your Movie Has been Uploaded',
+      
+            ],200);
+
+            
+    
+    }
+
+  
 }
